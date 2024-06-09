6cc524ee74918fe2989fda1fb8dde3b1c5a8a4fb
Fabio-Oliveira-Dev <fabiooliveiraideia2@gmail.com>
2024-06-09 19:23:17 -0300

    js dos botoes redes sociais

--- js/script.js
+++ js/script.js
@@ -1,3 +1,33 @@
+const whatsapp = document.getElementById("whatsapp");
+const instagram = document.getElementById("instagram");
+const gmail = document.getElementById("gmail");
+const facebook = document.getElementById("facebook");
+const youtube = document.getElementById("youtube");
+
+
+//botoes ancoras redes sociais
+function btn_redes(){
+    //whatsapp
+    let phone = 21978785267;
+    whatsapp.addEventListener("click", function(){
+        window.open(`https://wa.me/${phone}`, "_blank")
+    });
+
+    // Instagram
+    instagram.addEventListener("click", function(){
+        window.open(`https://www.instagram.com/klamil.ferramentas/`, "_blank")
+    });
+
+    //Gmail
+    const email = "nuyensrodrigo@gmail.com";
+    gmail.addEventListener("click", function(){
+        window.open(`https://mail.google.com/mail/?view=cm&fs=1&to=${email}`, "_blank")
+    });
+}
+btn_redes();
+
+
+// Carrosel
 let currentIndex = 0;
 const slides = document.querySelectorAll('.carousel-item');
 const totalSlides = slides.length;
@@ -26,3 +56,5 @@ setInterval(nextSlide, 5000); // Muda a imagem a cada 3 segundos
 
 // Inicialização
 showSlide(currentIndex);
+
+
